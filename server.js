0f18f324ee1373cec44ea574c7de9919eb89c137
vietwebgithub <nguyenquocviet2020ca@gmail.com>
2026-08-24 17:03:08 +0700

    update server

--- server.js
+++ server.js
@@ -122,5 +122,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server đang chạy tại: http://localhost:${PORT}`);
+  console.log(`Server đang chạy tại: https://chatserver-hhrp.onrender.com:${PORT}`);
 });
